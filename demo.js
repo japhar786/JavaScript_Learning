b7fdb6187015d457b5ce89db2b3fcbb38ea2da27
japhar786 <113526656+japhar786@users.noreply.github.com>
2022-10-04 06:18:17 -0400

    First program execution in JS
    First program execution in JS

---
+++ demo.js
@@ -0,0 +1,6 @@
+console.log("Japhar Dilawar Mujawar")
+console.info("My Hobbies are-1.Reading 2.Travelling 3.Drawing")
+console.log("My Strength- 1.Creativity. 2.Leadership skills 3.Ability to work in a team ")
+console.log("My Weaknesses- 1.Too Direct 2.Public Speaking 3.Insecure ")
+console.log("Result of addition of (30+50):  80");
+console.log("Result of multiplication of (20*20):  400");
